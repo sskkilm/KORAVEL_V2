c63e478a404231b0aff474027005e4e85644b02d
LSG <sskkilm@naver.com>
2024-09-19 11:33:50 +0900

    feat: add jasypt config

--- src/test/java/com/minizin/travel/JasyptTest.java
+++ src/test/java/com/minizin/travel/JasyptTest.java
@@ -1,11 +1,11 @@
-package com.minizin.travel.config;
+package com.minizin.travel;
 
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.iv.RandomIvGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class JasyptConfigTest {
+class JasyptTest {
 
     @Test
     @DisplayName("암호화 결과 출력")
